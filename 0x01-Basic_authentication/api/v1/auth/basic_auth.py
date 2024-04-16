48db5b9f0a860415c9d77e444370b3ef8116dfb0
AkinsolaTemidayo <temidayoakinsola100@gmail.com>
2024-04-16 17:17:57 +0000

    task_completed

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Basic authentication module for the API.
+"""
+import re
+import base64
+import binascii
+from typing import Tuple, TypeVar
+
+from .auth import Auth
+from models.user import User
+
+
+class BasicAuth(Auth):
+    """Basic authentication class.
+    """
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """Extracts the Base64 part of the Authorization header
+        for a Basic Authentication.
+
